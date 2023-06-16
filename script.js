35ceb7e5012c21b043bb0f8b9de0437b77b9ac28
Bedanta Dey <135055975+bedantadey17@users.noreply.github.com>
2023-06-16 14:25:54 +0530

    revert changes to 1.2.3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Good morning/day/evening!\nWe're running on version: 1.2.4")
+console.log("Good morning/day/evening!\nWe're running on version: 1.2.3")
 console.log("How to read version naming scheme: X.Y.Z\nZ is most minor release. As in very minor updates like bug fixes. \nY is a slightly major site update, with new features and stuff. \nX is a site release; will increment every Y=9 and Z=9 versions.")
 console.log("Website made by Bedanta Dey. All rights reserved. ")
 
