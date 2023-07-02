b78a98b34ec706f041f1bf5ef90d13db7798cc72
bedantadey17 <deybedanta1@gmail.com>
2023-07-02 18:45:07 +0530

    added unifsearch

---
+++ search/script.js
@@ -0,0 +1,30 @@
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', () => {
+  const searchInput = document.getElementById('searchInput').value;
+  const searchEngine = document.getElementById('searchEngine').value;
+
+  let url;
+  if (searchEngine === 'google') {
+    url = `https://www.google.com/search?q=${encodeURIComponent(searchInput)}`;
+  } else if (searchEngine === 'wikipedia') {
+    url = `https://en.wikipedia.org/wiki/${encodeURIComponent(searchInput)}`;
+  } else if (searchEngine === 'youtube') {
+    url = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchInput)}`;
+  } else if (searchEngine === 'bing') {
+    url = `https://www.bing.com/search?q=${encodeURIComponent(searchInput)}`;
+  } else if (searchEngine === 'ddg') {
+    url = `https://duckduckgo.com/?q=${encodeURIComponent(searchInput)}`;
+  }
+
+  window.open(url, '_blank');
+});
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+
+
