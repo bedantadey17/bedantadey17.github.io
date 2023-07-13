b832f6a52633d50b8ed66ae94e843cbd363e8ece
Bedanta Dey <135055975+bedantadey17@users.noreply.github.com>
2023-07-13 23:59:26 +0530

    theme overhaul

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-console.log("Good morning/day/evening!\nWe're running on version: 1.3.5")
-console.log("How to read version naming scheme: X.Y.Z\nZ is most minor release. As in very minor updates like bug fixes. \nY is a slightly major site update, with new features and stuff. \nX is a site release; will increment every Y=9 and Z=9 versions.")
-console.log("Website made by Bedanta Dey and Gray Matter RI. All rights reserved.")
-console.log("Wondering what happened to Newsletter? Go to: bedantadey.github.io/archive/letter")
+console.log("Good morning/day/evening!\nWe're running on version: 1.3.6")
 
 
 const modePreference = localStorage.getItem('darkMode');
@@ -19,23 +16,49 @@ darkModeToggle.addEventListener('click', () => {
   localStorage.setItem('darkMode', isDarkMode);
 });
 
+// TIME
 
-// splash
+document.addEventListener("DOMContentLoaded",()=>{
+  let ho;
+  let CT = document.getElementById("currentTime");
+  CT.innerHTML = r();
+  clearInterval(ho);
+  ho = setInterval(() => {
+      CT.innerHTML = r();
+  }, 1000)
+  function r(){
+      const time = new Date();
+      return "-- Time for me: " + Intl.DateTimeFormat("en-GB", where).format(time) + " --";
+  }
+})
 
-window.addEventListener("DOMContentLoaded", function () {
-  fetch("text-data.json")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var texts = data.texts;
-      var randomIndex = Math.floor(Math.random() * texts.length);
-      var dynamicText = texts[randomIndex];
+const where = {
+  timeZone: "Asia/Calcutta",
+  hour12: true,
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  dayPeriod: "long"
+}
 
-      var dynamicTextElement = document.getElementById("dynamicText");
-      dynamicTextElement.textContent = dynamicText;
-    })
-    .catch(function (error) {
-      console.log("Failed to fetch text data:", error);
-    });
-});
+const options = {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+};
+
+// MOUSE MOVE
+
+let rr = document.createElement("div");
+rr.id = "mou";
+rr.position = "fixed";
+document.body.appendChild(rr);
+
+
+document.addEventListener('mousemove', (e)=>{
+    let locX = e.clientX;
+    let locY = e.clientY;
+    let mouse = document.getElementById("mou");
+    mouse.style.left = locX + "px";
+    mouse.style.top = locY + "px" 
+})
