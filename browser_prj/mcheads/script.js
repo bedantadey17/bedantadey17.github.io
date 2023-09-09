22a96b1097f0a407be9d00d53f4ff927ea4a9be1
Bedanta Dey <135055975+bedantadey17@users.noreply.github.com>
2023-09-09 23:12:41 +0530

    much needed update

--- browser_prj/mcheads/script.js
+++ browser_prj/mcheads/script.js
@@ -1,24 +1,74 @@
+// head with layer
+
 function showHead() {
     var username = document.getElementById('mc-username').value;
     var headDiv = document.getElementById('mc-head');
+    var headSize = document.getElementById('mc-size').value;
+
     if (username.length > 0) {
+        if (headSize === "") {
+            alert("Please enter a head size.");
+            return;
+        }
         headDiv.style.display = 'block';
-        headDiv.style.backgroundImage = `url('https://minotar.net/helm/${username}/150.png')`;
+        headDiv.style.backgroundImage = `url('https://mc-heads.net/avatar/${username}/${headSize}')`;
         document.getElementById('save-button').style.display = 'block';
     }
 }
+
+// head without layer
+
+function showHeadNaked() {
+    var username = document.getElementById('mc-username').value;
+    var headDiv = document.getElementById('mc-head');
+    var headSize = document.getElementById('mc-size').value;
+
+    if (username.length > 0){
+        if (headSize === "") {
+            alert("Please enter a head size.");
+            return;
+        }
+        headDiv.style.display = 'block';
+        headDiv.style.backgroundImage = `url('https://mc-heads.net/avatar/${username}/${headSize}/nohelm')`;
+        document.getElementById('save-button').style.display = 'block';
+    }
+}
+
+// cube head
 
 function showSkull() {
     var username = document.getElementById('mc-username').value;
     var headDiv = document.getElementById('mc-head');
+    var headSize = document.getElementById('mc-size').value;
 
     if (username.length > 0) {
+        if (headSize === "") {
+            alert("Please enter a head size.");
+            return;
+        }
         headDiv.style.display = 'block';
-        headDiv.style.backgroundImage = `url('https://minotar.net/avatar/${username}/150.png')`;
+        headDiv.style.backgroundImage = `url('https://mc-heads.net/head/${username}/${headSize}')`;
         document.getElementById('save-button').style.display = 'block';
     }
 }
 
+// cube head no layer
+
+function showSkullNaked() {
+    var username = document.getElementById('mc-username').value;
+    var headDiv = document.getElementById('mc-head');
+    var headSize = document.getElementById('mc-size').value;
+
+    if (username.length > 0) {
+        if (headSize === "") {
+            alert("Please enter a head size.");
+            return;
+        }
+        headDiv.style.display = 'block';
+        headDiv.style.backgroundImage = `url('https://mc-heads.net/head/${username}/${headSize}/nohelm')`;
+        document.getElementById('save-button').style.display = 'block';
+    }
+}
 
 function saveImage() {
   var headDiv = document.getElementById('mc-head');
