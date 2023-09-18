1f599516cb245b49c5601af2acac3d6d79fedfdb
Andrie <abalanandreibalan8@yahoo.com>
2023-09-18 19:43:35 +0300

    I used the wrong system for the duration :/

--- script.js
+++ script.js
@@ -79,5 +79,5 @@ document.addEventListener('mousemove', (e)=>{
     let mouse = document.getElementById("mou");
     // mouse.style.left = locX + "px";
     // mouse.style.top = locY + "px"
-    mouse.animate({ left: `${locX}px`, right: `${locY}px` }, {duration: "0.2s"})
+    mouse.animate({ left: `${locX}px`, right: `${locY}px` }, {duration: 0.2 * 1000})
 })
